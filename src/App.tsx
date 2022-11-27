0f600f34477c88433bfff5b29869f19cecdb9bac
dylan <dylanc.codcazeraz>
2022-11-27 17:38:35 +0100

    :sparkles: added new quote button

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import ErrorMessage from './ErrorMessage'
 import { AxiosError } from 'axios'
 
 const App = () => {
-  const { data, error, isError, isLoading, isSuccess } = useQuery<Quote, AxiosError>('quote', getRandomQuote)
+  const { data, error, isError, isLoading, isSuccess, refetch } = useQuery<Quote, AxiosError>('quote', getRandomQuote)
 
   return (
     <div>
       {isLoading && <p>Is Loading ...</p>}
       {isError && <ErrorMessage error={error} />}
       {isSuccess && <QuoteComponent quote={data} />}
+      <button onClick={() => refetch()}>New Quote</button>
     </div>
   )
 }
